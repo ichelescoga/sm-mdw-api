44c521fb987cf112f3a3148a28aaf002f29603f7
Gustavo Ichel <ichelescoga@gmail.com>
2023-02-20 17:13:30 -0600

    parse int in parameters

--- controller/orders.controller.js
+++ controller/orders.controller.js
@@ -233,8 +233,8 @@ exports.getAllMiddlewareOrdersByStore = async(req, res, next)=>{
     try {
         let params = {}
             params.storeId = req.params.storeId
-            params.initialDate = new Date (req.params.initialDate * 1000)
-            params.endDate = new Date (req.params.endDate * 1000)
+            params.initialDate = new Date (parseInt(eq.params.initialDate) * 1000)
+            params.endDate = new Date (parseInt(req.params.endDate) * 1000)
         let mdwOrders = await OrderRepository.getAllMdwOrdersByStore(params);          
         res.json(mdwOrders)
         
